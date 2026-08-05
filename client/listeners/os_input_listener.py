f9874ec822b4862a5afba7ecc3fefeac185f7d81
Kenneth <agonoy@ualberta.ca>
2026-08-04 22:01:37 -0600

    finished os_input

--- client/listeners/os_input_listener.py
+++ client/listeners/os_input_listener.py
@@ -1,9 +1,9 @@
 import socket
 import json
 import time
+import threading 
 from datetime import datetime, timezone 
 from pynput import mouse, keyboard
-
 
 TARGET_HOST = "127.0.0.1"
 TARGET_PORT = 5000
@@ -12,96 +12,124 @@ SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 8000 
 
 class InputRecording:
-    def __init__(self) -> None:
-        self.os_input_events = []
+    def __init__(self, on_event = None) -> None:
+        self._on_event = on_event
+        self._enabled = threading.Event()
+        self._enabled.set()
+
+        self.mouse_listener = mouse.Listener(on_click=self.on_click)
+        self.keyboard_listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+
+    def enable(self):
+        self._enabled.set()
+
+    def disable(self):
+        self._enabled.clear()
+
+    def is_enabled(self):
+        return self._enabled.is_set()
+
+    def start(self):
+        self.mouse_listener.start()
+        self.keyboard_listener.start()
+
+    def stop(self):
+        self.mouse_listener.stop()
+        self.keyboard_listener.stop()
+
+        self.mouse_listener.join()
+        self.keyboard_listener.join()
+    
+    def send_event(self, event):
+        if not self._enabled.is_set():
+            return
+
+        if self._on_event is not None:
+            try: 
+                self._on_event(event)
+            except Exception as error:
+                print(f"{error}")
 
     def on_click(self, x, y , mouse_button, is_pressed):
 
-    
+        if not is_pressed:
+            return 
 
-        if is_pressed:
-            event_name = "mouse_event"
-            client_time = get_client_time()
-            coordinates = (f"x-pos: {x}, y-pos: {y}")
-            self.os_input_events.append([client_time, "client", event_name, coordinates, mouse_button])
+        self.send_event({
+            "client_time": get_client_time(),
+            "source": "client",
+            "event": f"mouse_click",
+            "data": {
+                "x-pos:": x,
+                "y-pos:": y,
+                "mouse_button": mouse_button.name,
+            }
+        })
 
     def on_press(self, key):
 
-        client_time = get_client_time()
-
-        try:  
-            event_name = f"keyboard_pressed_{key.char}"
-            self.os_input_events.append([client_time, "client", event_name])
+        try:
+            key_name = key.char
         except AttributeError:
-            event_name = f"keyboard_pressed_{key}"
-            self.os_input_events.append([client_time, "client", event_name])
+            key_name = str(key).split(".")[-1]
+
+        self.send_event({
+            "client_time": get_client_time(),
+            "source": "client",
+            "event": f"keyboard_press_{key_name}",
+            "data": {
+                "key": key_name
+            }
+        })
 
     def on_release(self, key):
-        """ temporary function that should be moved onto main """
+        if not self._enabled.is_set():
+            return
 
-        event_type = "keyboard_event"
+        try: 
+            key_name = key.char
+        except AttributeError:
+            key_name = str(key).split(".")[-1]
 
-        if key == keyboard.Key.esc:
-            client_time = get_client_time()
-            self.os_input_events.append([client_time, event_type, key])
-            return False
+        self.send_event({
+            "client_time": get_client_time(),
+            "source": "client",
+            "event": f"keyboard_release_{key_name}",
+            "data": {
+                "key": key_name
+            }
+        })
 
 def get_client_time():
     return datetime.now(timezone.utc).isoformat()
 
-def send_log(event_name ,coordinates):
+def repackage_payload(payload):
 
-    payload = {
-        "client_time": datetime.now(timezone.utc).isoformat(),
+    return {
+        "client_time": get_client_time(),
         "source": "client",
-        "event": event_name,
-        "data": {'coordinates': coordinates}
+        "event": payload.get("event"),
+        "data": payload.get("data")
     }
-    
-    data = json.dumps(payload).encode("UTF-8")
-
-    try:
-
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            sock.sendto(data, (SERVER_HOST, SERVER_PORT))
-
-    except Exception as e:
-        print(f"Failed to send UDP packet: {e}")
-
 
 def main(): 
-    
-    recorder = InputRecording()
 
-    mouse_listener = mouse.Listener(on_click=recorder.on_click)
-    keyboard_listener = keyboard.Listener(on_press=recorder.on_press, on_release=recorder.on_release)
+    def on_event(event):
+        print(f"Event received: {event}")
 
-    mouse_listener.start()
-    keyboard_listener.start()
+    recorder = InputRecording(on_event=on_event)
+    recorder.start()
+    print("Started.")
 
-    print("checkpoint1: listener started")
-
-    try: 
-        while keyboard_listener.running and mouse_listener.running: 
+    try:
+        while recorder.keyboard_listener.running and recorder.mouse_listener.running:
             time.sleep(0.1)
-
     except KeyboardInterrupt:
         print("Stop it.")
-
     finally:
-        mouse_listener.stop()
-        keyboard_listener.stop()
+        recorder.stop()
 
-        keyboard_listener.join()
-        mouse_listener.join()
-
-    print("checkpoint2: print the values")
-
-    # temporary just to get it working and then will integrate it live 
-    for event in recorder.os_input_events:
-        print(event)
-
-    print("checkpoint3: script end")
+    print("End.") 
 
 
 if __name__ == "__main__":
--- client/listeners/target_listener.py
+++ client/listeners/target_listener.py
@@ -1,57 +1,17 @@
 import socket
 import json
-import struct
 from datetime import datetime, timezone
-
-TARGET_HOST = "127.0.0.1"
-TARGET_PORT = 5000
-
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 8000 
 
 MAX_PACKET_SIZE = 65535
 
-def server_connect(host, port):
+def target_connect(host, port):
 
-    try:
+    target_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    target_sock.bind((host, port))
+    
+    print(f"[CLIENT TO TARGET] Listening: {host}:{port}") 
 
-        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_sock.connect((host, port))
-
-        print(f"[SERVER] Connection successful: {host}:{port}")
-
-        return server_sock
-
-    except ConnectionRefusedError:
-
-        print(f"[SERVER] Connection refused: {host}:{port}")
-        print("[SERVER] Data will NOT be forwarded.")
-
-        return None
-
-    except OSError as e:
-
-        print(f"[SERVER] Connection failed: {host}:{port} ==> {e}")
-        print("[SERVER] Data will NOT be forwarded.")
-
-        return None
-
-def server_send(sock, payload):
-   
-    try:
-
-        data = json.dumps(payload).encode("UTF-8")
-        header = struct.pack(">I", len(data))
-
-        sock.sendall(header + data)
-
-        return True
-
-    except (ConnectionResetError, OSError) as e:
-
-        print(f"[SERVER] Data failed to send due to error: {e}")
-
-        return False
+    return target_sock
 
 def repackage_payload(payload):
 
@@ -66,53 +26,25 @@ def repackage_payload(payload):
         "data": payload.get("data")
     }
 
-def main():
+def listen(target_sock):
+   
+    try: 
+        data, addr = target_sock.recvfrom(MAX_PACKET_SIZE)
 
-    target_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    target_sock.bind((TARGET_HOST, TARGET_PORT))
+        payload = json.loads(data.decode("UTF-8"))
+        payload = repackage_payload(payload)
 
-    print(f"[CLIENT TO TARGET] Listening: {TARGET_HOST}:{TARGET_PORT}")
+        return payload, addr
 
-    server_sock = server_connect(SERVER_HOST, SERVER_PORT)
+    except json.JSONDecodeError: 
 
-    try:
-        while True:
+        print("\n=== INVALID JSON RECEIVED ===")
+        return None, None
 
-            data, addr = target_sock.recvfrom(MAX_PACKET_SIZE)
+    except OSError as e:
+        print("=== SOCKET ERROR {e} ===")
+        return None, None
 
-            try:
+  
 
-                payload = json.loads(data.decode("UTF-8"))
-
-                payload = repackage_payload(payload)
-
-                print("\n=== TARGET PACKET RECEIVED ===")
-                print(f"From : {addr}")
-                print(json.dumps(payload, indent=2))
-
-                if server_sock is not None:
-                    server_send(server_sock, payload)
-                    print(f"[SERVER] Payload sent to {SERVER_HOST}:{SERVER_PORT}")
-                else:
-                    print("[SERVER] No server connection.")
-
-            except json.JSONDecodeError:
-
-                print("\n=== INVALID JSON RECEIVED ===")
-                print(f"From: {addr}")
-                print(data)
-
-    except KeyboardInterrupt:
-
-        print("\n [CLIENT] Stop it.")
-
-    finally:
-
-        if target_sock is not None:
-            target_sock.close()
-
-        if server_sock is not None:
-            server_sock.close()
-
-if __name__ == "__main__":
-    main()
+    
